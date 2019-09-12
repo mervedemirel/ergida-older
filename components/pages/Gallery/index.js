8d010b78de42d13c1b8484793246668744ad158f
mustafa <balcimustafa1@gmail.com>
2019-09-12 23:10:57 +0300

    en hero 2

--- components/pages/Gallery/index.js
+++ components/pages/Gallery/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Hero from "../Homepage/Hero/Hero";
 import GalleryPhotos from "./GalleryPhotos/GalleryPhotos";
 import HomeCta from "../Homepage/HomeCta/HomeCta";
+import { i18n } from "../../../i18n";
+
 
 const GalleryIndex = () => {
     return (
         <>
-            <Hero bigImage="../../../../static/images/galeri-hero.jpg"
-                  mobImage="../../../../static/images/galeri-hero-mob.jpg"/>
+            {i18n.language === 'tr' ? <Hero bigImage="../../../../static/images/galeri-hero.jpg"
+                mobImage="../../../../static/images/galeri-hero-mob.png" /> :
+                <Hero bigImage="../../../../static/images/galeri-hero-en.jpg"
+                    mobImage="../../../../static/images/galeri-hero-mob.png" />}
             <GalleryPhotos/>
             <HomeCta className="mt-5"/>
         </>
--- components/pages/Vegetable/index.js
+++ components/pages/Vegetable/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from "../Homepage/Hero/Hero";
 import VegContainer from "./VegContainer/VegContainer";
+import { i18n } from "../../../i18n";
+
 
 const VegetablePageIndex = () => {
     return (
         <>
-            <Hero bigImage="../../../../static/images/sebze-hero.jpg"
-                  mobImage="../../../../static/images/sebze-hero-mob.jpg"/>
+        {i18n.language === 'tr' ? <Hero bigImage="../../../../static/images/sebze-hero.jpg"
+                mobImage="../../../../static/images/sebze-hero-mob.png" /> :
+                <Hero bigImage="../../../../static/images/sebze-hero-en.jpg"
+                    mobImage="../../../../static/images/sebze-hero-mob.png" />}
             <VegContainer/>
         </>
     );
