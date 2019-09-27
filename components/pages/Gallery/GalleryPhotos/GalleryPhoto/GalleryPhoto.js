7ab581f92e5b59537f30eb4aee9c18e74d733ee5
mustafa <balcimustafa1@gmail.com>
2019-09-27 09:42:57 +0300

    gallery

--- components/pages/Gallery/GalleryPhotos/GalleryPhoto/GalleryPhoto.js
+++ components/pages/Gallery/GalleryPhotos/GalleryPhoto/GalleryPhoto.js
@@ -3,19 +3,38 @@ import React from 'react';
 const GalleryPhoto = (props) => {
     return (
         <div className="Gallery-card relative">
-            <img className="Gallery-image" src={'../../../../static/images/' + props.imageName} alt=""/>
+            <img className="Gallery-image" src={`https://admin.ergidatarim.com.tr${props.imageName}`}
+                 alt={props.altTag}/>
             <style jsx>{`
                 .Gallery-image {
                     width: 100%;
                     transform: translate(1.5rem, 1.5rem);
                     transition: transform .5s;
                     box-shadow: 0 0 10px;
-
+                    width: 236px;
+                    height: 333px;
+                    object-fit: cover;
                 }
                 
                 @media (max-width: 767px) {
                     .Gallery-image {
+                        width: 100%;
+                        height: auto;
                         transform: none;
+                    }
+                }
+                
+                @media (min-width:768px) and (max-width: 992px) {
+                    .Gallery-image {
+                        width: 306px;
+                        height: 340px;
+                    }
+                }
+                
+                @media (min-width:992px) and (max-width: 1199px) {
+                    .Gallery-image {
+                        width: 192px;
+                        height: 340px;
                     }
                 }
                 
--- components/pages/Gallery/GalleryPhotos/GalleryPhotos.js
+++ components/pages/Gallery/GalleryPhotos/GalleryPhotos.js
@@ -1,88 +1,103 @@
-import React from 'react';
+import React, {Component} from 'react';
 import GalleryPhoto from "./GalleryPhoto/GalleryPhoto";
+import axios from "axios";
 
-const GalleryPhotos = () => {
-    return (
-        <>
-            <div className="container-fluid p-x-0 py-5 Gphoto">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="container-fluid p-x-0 py-5 Gphoto">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
+class GalleryPhotos extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            photos: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("https://admin.ergidatarim.com.tr/galleries", {
+            headers: {
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTY4MTQyNjQzLCJleHAiOjE1NzA3MzQ2NDN9.tqzTZksZT1lCQ38OSPVZd-VRIuja6VXrGahasTQUI7c`,
+            }
+        }).then(res => {
+            const photos = res.data
+            this.setState({photos})
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <div className="container-fluid p-x-0 py-5 Gphoto">
+                    <div className="container">
+                        <div className="row">
+                            {this.state.photos.slice(0,4).map((photo, i) => {
+                                return (
+                                    <div className="col-md-6 col-lg-3 p-3" key={i}>
+                                        <GalleryPhoto imageName={photo.photo.url} altTag={photo.alt_tag} />
+                                    </div>
+                                )
+                            })}
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="container-fluid p-x-0 py-5 Gphoto">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
+                <div className="container-fluid p-x-0 py-5 Gphoto">
+                    <div className="container">
+                        <div className="row">
+                            {this.state.photos.slice(4,8).map((photo, i) => {
+                                return (
+                                    <div className="col-md-6 col-lg-3 p-3" key={i}>
+                                        <GalleryPhoto imageName={photo.photo.url} altTag={photo.alt_tag} />
+                                    </div>
+                                )
+                            })}
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="container-fluid p-x-0 py-5 Gphoto">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
-                        </div>
-                        <div className="col-md-6 col-lg-3 p-3">
-                            <GalleryPhoto imageName="avokadox.png" />
+                <div className="container-fluid p-x-0 py-5 Gphoto">
+                    <div className="container">
+                        <div className="row">
+                            {this.state.photos.slice(8,12).map((photo, i) => {
+                                return (
+                                    <div className="col-md-6 col-lg-3 p-3" key={i}>
+                                        <GalleryPhoto imageName={photo.photo.url} altTag={photo.alt_tag} />
+                                    </div>
+                                )
+                            })}
                         </div>
                     </div>
                 </div>
-            </div>
-            <style jsx>{`
+                <div className="container-fluid p-x-0 py-5 Gphoto">
+                    <div className="container">
+                        <div className="row">
+                            {this.state.photos.slice(12,16).map((photo, i) => {
+                                return (
+                                    <div className="col-md-6 col-lg-3 p-3" key={i}>
+                                        <GalleryPhoto imageName={photo.photo.url} altTag={photo.alt_tag} />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <div className="container-fluid p-x-0 py-5 Gphoto">
+                    <div className="container">
+                        <div className="row">
+                            {this.state.photos.slice(16,20).map((photo, i) => {
+                                return (
+                                    <div className="col-md-6 col-lg-3 p-3" key={i}>
+                                        <GalleryPhoto imageName={photo.photo.url} altTag={photo.alt_tag} />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <style jsx>{`
                 .Gphoto:nth-child(even) {
                     background: #f8f8f8;
                 }
             `}</style>
-        </>
-    );
-};
+            </>
+        );
+    }
+}
 
 export default GalleryPhotos;
