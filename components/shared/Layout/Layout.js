8de858de2b28483c97a1ee14415696f91c609d24
mustafa <balcimustafa1@gmail.com>
2019-10-03 08:59:25 +0300

    webfont fix

--- components/shared/Layout/Layout.js
+++ components/shared/Layout/Layout.js
@@ -24,14 +24,6 @@ const Layout = ({children, t}) => {
         setMenuIsOpen(!menuIsOpen)
     };
 
-    useEffect(() => {
-        WebFont.load({
-            google: {
-                families: ['Open Sans:300,400,600, 700']
-            }
-        })
-    })
-
     return (
         <>
             <Menu left customBurgerIcon={false} isOpen={menuIsOpen} disableAutoFocus>
--- components/shared/Leaves/Leaves.js
+++ components/shared/Leaves/Leaves.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, {useRef, useEffect, useState} from 'react';
+import {TweenMax, TwenLite} from 'gsap';
 
-import { TweenLite, TweenMax } from "gsap";
 
-class Leaves extends Component {
-    constructor(props) {
-        super(props);
-    }
+const Leaves = () => {
+    let animElement = useRef(null)
+    const [leaves, setLeaves] = useState([])
 
-    componentDidMount() {
-    }
+    const funcR = (min,max) => min+Math.random()*(max-min);
 
-    render() {
-        return (
-            <div></div>
-        );
-    }
-}
+    useEffect(() => {
+        TweenLite.set(animElement, {perspective: 600})
+
+        let total = 30;
+        const leavesArr = [];
+        for (i=0; i<total; i++){
+            var Div = document.createElement('div');
+            TweenLite.set(Div,{attr:{class:'dot'},x: funcR(0,w),y: funcR(-200,-150),z: funcR(-200,200)});
+            leavesArr.push(Div);
+            // animm(Div);
+        }
+        setLeaves(leavesArr);
+    })
+
+    return (
+        <div ref={element => {animElement = element}} >
+            {leavesArr.map((item, i) => {
+                return item
+            })}
+        </div>
+    );
+};
 
 export default Leaves;
+
