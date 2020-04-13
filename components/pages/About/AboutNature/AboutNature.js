447e12d7424024804d3f47148fea14748982444e
mervedemirel <mervedemirel41@hotmail.com>
2020-04-13 20:11:29 +0300

    Yaprak animasyonu eklendi.

--- components/pages/About/AboutNature/AboutNature.js
+++ components/pages/About/AboutNature/AboutNature.js
@@ -8,6 +8,9 @@ const AboutNature = ({t}) => {
             <div className="row">
                 <div className="col-md-6 text-center text-md-right flex-column d-flex justify-content-center">
                     <h2><span className="er-darkest-green font-weight-bold green-title">{t('natureTitleTop')}</span><br/> {t('natureTitleBot')}</h2>
+                        <div className="leaf">
+                            <img src="../../../../static/images/leaf.png" className="animation"/>
+                        </div> 
                     <p>{t('natureText')}</p>
                 </div>
                 <div className="col-md-6 d-flex align-items-center">
@@ -21,12 +24,18 @@ const AboutNature = ({t}) => {
                 </div>
                 <div className="col-md-6 d-flex flex-column justify-content-center">
                     <h3><span className="er-darkest-green font-weight-bold green-title">{t('countryTitleTop')}</span> <br/> {t('countryTitleBot')}</h3>
+                        <div className="leaf">
+                            <img src="../../../../static/images/leaf.png" className="animation"/>
+                        </div> 
                     <p className="mt-3">{t('countryText')}</p>
                 </div>
             </div>
             <div className="row mt-5 flex-column-reverse flex-md-row">
                 <div className="col-md-6">
                     <h3 className="mt-5 mt-md-0"><span className="er-darkest-green font-weight-bold green-title">{t('globalTitle')}</span></h3>
+                        <div className="leaf">
+                            <img src="../../../../static/images/leaf.png" className="animation"/>
+                        </div> 
                     <p className="mt-3">{t('globalText')}</p>
 
                     <h4 className="mt-5 green-title er-darkest-green">{t('missionTitle')}</h4>
@@ -47,6 +56,32 @@ const AboutNature = ({t}) => {
                     hr {
                         display: none;
                     }
+                }
+
+                .animation{
+                  animation: animate 10s;
+                  animation-iteration-count: infinite;
+                }
+
+                .leaf img{
+                    position: absolute;
+                    left: 300px;
+                    top: 150px;
+                    z-index: -1;
+                }
+
+                @keyframes animate {
+                  0% { transform: translate(1px, 1px) rotate(-10deg); }
+                  10% { transform: translate(-1px, -2px) rotate(-10deg); }
+                  20% { transform: translate(-3px, 0px) rotate(0deg); }
+                  30% { transform: translate(3px, 2px) rotate(0deg); }
+                  40% { transform: translate(1px, -1px) rotate(10deg); }
+                  50% { transform: translate(-1px, 2px) rotate(0deg); }
+                  60% { transform: translate(-3px, 1px) rotate(0deg); }
+                  70% { transform: translate(3px, 1px) rotate(-10deg); }
+                  80% { transform: translate(-1px, -1px) rotate(0deg); }
+                  90% { transform: translate(1px, 2px) rotate(0deg); }
+                  100% { transform: translate(1px, -2px) rotate(-10deg); }
                 }
             `}</style>
         </div>
