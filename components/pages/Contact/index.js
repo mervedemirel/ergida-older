8c1a4229c7dc7815738d9a2f054c8f9588375da3
mervedemirel <mervedemirel41@hotmail.com>
2020-04-16 18:28:42 +0300

    Yaprak animasyonu eklendi.

--- components/pages/Contact/index.js
+++ components/pages/Contact/index.js
@@ -31,8 +31,49 @@ const ContactPageIndex = () => {
             <Delayed waitBeforeShow={1023}>
                 <Leaves top="140vh" left="10vw"/>
             </Delayed>
+            <div className="row justify-content-between">
+                <div class="leaf col-xs-auto container-fluid">
+                    <img src="../../../static/images/leaf.png" className="animation mx-auto d-block"/>
+                </div>
+                <div class="leaf col-xs-auto container-fluid">
+                    <img src="../../../static/images/leaf.png" className="animation mx-auto d-block"/>
+                </div>
+            </div>
             <Map/>
             <ContactForm/>
+            <style jsx>{`
+                .animation{
+                  animation: animate 10s;
+                  animation-iteration-count: infinite;
+                }
+
+                .leaf{
+                    height:120px;
+                }
+
+                .container-fluid{
+                    width: 100%;
+                }
+
+                .leaf img{
+                    position: absolute;
+                    z-index: -1;
+                }
+
+                @keyframes animate {
+                  0% { transform: translate(1px, 1px) rotate(-10deg); }
+                  10% { transform: translate(-1px, -2px) rotate(-10deg); }
+                  20% { transform: translate(-3px, 0px) rotate(0deg); }
+                  30% { transform: translate(3px, 2px) rotate(0deg); }
+                  40% { transform: translate(1px, -1px) rotate(10deg); }
+                  50% { transform: translate(-1px, 2px) rotate(0deg); }
+                  60% { transform: translate(-3px, 1px) rotate(0deg); }
+                  70% { transform: translate(3px, 1px) rotate(-10deg); }
+                  80% { transform: translate(-1px, -1px) rotate(0deg); }
+                  90% { transform: translate(1px, 2px) rotate(0deg); }
+                  100% { transform: translate(1px, -2px) rotate(-10deg); }
+                }
+            `}</style>
         </>
     );
 };
