4b6e28f8086ee2690741f697a04c4d45215ea3ad
mervedemirel <mervedemirel41@hotmail.com>
2020-04-16 18:24:41 +0300

    Update AboutPrinciple.js

--- components/pages/About/AboutPrinciple/AboutPrinciple.js
+++ components/pages/About/AboutPrinciple/AboutPrinciple.js
@@ -7,9 +7,6 @@ const AboutPrinciple = ({t}) => {
             <div className="row">
                 <div className="col-md-6 text-center text-md-right flex-column d-flex justify-content-center">
                     <h2><span className="er-darkest-green font-weight-bold green-title">{t('principleTopTitle')}</span><br/> {t('principleBotTitle')}</h2>
-                        <div className="leaf col-md-1">
-                            <img src="../../../../static/images/leaf.png" className="animation mx-auto d-block"/>
-                        </div>
                     <p>{t('principleTextTop')} <br/> <strong>{t('principleTextBot')}</strong></p>
                 </div>
                 <div className="col-md-6 d-flex align-items-center">
@@ -23,9 +20,6 @@ const AboutPrinciple = ({t}) => {
                 </div>
                 <div className="col-md-6">
                     <h3><span className="er-darkest-green font-weight-bold green-title">{t('expTopTitle')}</span> <br/> {t('expBotTitle')}</h3>
-                        <div className="leaf col-md-1">
-                            <img src="../../../../static/images/leaf.png" className="animation mx-auto d-block"/>
-                        </div>
                     <p className="mt-3">{t('expText')}</p>
 
                     <h4 className="mt-5 green-title er-darkest-green">{t('ilkeTitle')}</h4>
@@ -43,32 +37,6 @@ const AboutPrinciple = ({t}) => {
                     hr {
                         display: none;
                     }
-                }
-        
-                .animation{
-                  animation: animate 10s;
-                  animation-iteration-count: infinite;
-                }
-
-                .leaf img{
-                    position: absolute;
-                    left: 300px;
-                    top: 150px;
-                    z-index: -1;
-                }
-
-                @keyframes animate {
-                  0% { transform: translate(1px, 1px) rotate(-10deg); }
-                  10% { transform: translate(-1px, -2px) rotate(-10deg); }
-                  20% { transform: translate(-3px, 0px) rotate(0deg); }
-                  30% { transform: translate(3px, 2px) rotate(0deg); }
-                  40% { transform: translate(1px, -1px) rotate(10deg); }
-                  50% { transform: translate(-1px, 2px) rotate(0deg); }
-                  60% { transform: translate(-3px, 1px) rotate(0deg); }
-                  70% { transform: translate(3px, 1px) rotate(-10deg); }
-                  80% { transform: translate(-1px, -1px) rotate(0deg); }
-                  90% { transform: translate(1px, 2px) rotate(0deg); }
-                  100% { transform: translate(1px, -2px) rotate(-10deg); }
                 }
             `}</style>
         </div>
