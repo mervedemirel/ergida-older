c7c65a33f2ea6478dd8ab80d5d5e904610df3629
Merve <mervedemirel41@hotmail.com>
2023-11-14 21:08:12 +0300

    update

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const nextI18NextMiddleware = require('next-i18next/middleware').default;
 
 const nextI18next = require("./i18n");
 
-const port = 5000;
+//const port = 5000;
+const port = process.env.PORT || 5000;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
@@ -14,7 +15,17 @@ const handle = app.getRequestHandler();
 
     server.use(nextI18NextMiddleware(nextI18next));
 
-    server.get('*', (req, res) => handle(req, res));
+    //server.get('*', (req, res) => handle(req, res));
+    server.get('*', (req, res) => {
+        // Ensure that the locale is set based on the request
+        req.i18n.changeLanguage(req.query.lang || 'tr');
 
-    await server.listen(port);
+        return handle(req, res);
+    });
+
+    //await server.listen(port);
+    await server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+    });
 })();
